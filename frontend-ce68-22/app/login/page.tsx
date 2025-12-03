38e27accfee82d61dc98c2237af0ad586b19c716
Hinoshii <captainthapat@gmail.com>
2025-12-03 13:16:42 +0700

    Add login and registration forms with basic functionality

---
+++ frontend-ce68-22/app/login/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginForm from '../components/LoginForm'
+
+const Login = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center p-6">
+      <LoginForm />
+    </div>
+  )
+}
+
+export default Login
