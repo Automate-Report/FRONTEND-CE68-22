f68299e7ae3a78f6591e35df168a963f9dac93ce
Nattharika <Nattharika25kata@gmail.com>
2025-12-05 18:31:28 +0700

    แก้ไข syntax

--- frontend-ce68-22/src/components/projects/icon/EditProject.tsx
+++ frontend-ce68-22/src/components/projects/icon/EditProject.tsx
@@ -2,9 +2,9 @@ export default function EditProjectIcon()
 {
     return (
        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M3 21H21" stroke="#404F57" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M7 17V13L17 3L21 7L11 17H7Z" stroke="#404F57" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M14 6L18 10" stroke="#404F57" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M3 21H21" stroke="#404F57" strokeWidth="2" strokeLinecap="round" stroke-linejoin="round"/>
+            <path d="M7 17V13L17 3L21 7L11 17H7Z" stroke="#404F57" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M14 6L18 10" stroke="#404F57" strokeWidth="2" strokeLinecap="round" stroke-linejoin="round"/>
         </svg>
     );
 }
