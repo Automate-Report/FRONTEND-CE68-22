92c208fa7ec9f8ec94a56dd5dfd1e3c3f5c26d61
Nattharika <Nattharika25kata@gmail.com>
2025-12-06 23:03:16 +0700

    ui get-project-by-id ยกเว้น dashboard แล้วก็กดมาหน้า main ได้

--- frontend-ce68-22/src/app/(project)/projects/overview/[id]/page.tsx
+++ frontend-ce68-22/src/app/(project)/projects/overview/[id]/page.tsx
@@ -3,12 +3,13 @@
 import { use, useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useProject } from "@/src/hooks/use-project";
+import { GenericBreadcrums } from "@/src/components/Common/GenericBreadCrums";
 
 interface PageProps{
     params: Promise<{ id: string}>;
 }
 
-export default function ProjectsDetailsPage({ params }: PageProps) {
+export default function ProjectsOverviewPage({ params }: PageProps) {
     const router = useRouter();
 
     const resolvePrams = use(params);
@@ -19,12 +20,27 @@ export default function ProjectsDetailsPage({ params }: PageProps) {
     if (isLoading) return <div className="p-8">Loading...</div>;
     if (isError || !project) return <div className="p-8 text-red-500">Project not found</div>;
 
+    const breadcrumbItems = [
+        { label: "Home", href: "/main"},
+        { label: project.name , href: undefined}
+    ];
+
+
+
     return (
         <div className="mx-auto w-11/12 bg-[#0F1518] text-[#E6F0E6]">
-            <div>
-                header
+            <GenericBreadcrums items={breadcrumbItems}/>
+            <div className="">
+                <div className="font-bold text-[36px] pb-3">
+                    What's this project about?
+                </div>
+                <div className="bg-[#FBFBFB] text-[#404F57] border rounded-2xl p-5 wrap-break-word">
+                    {project.description}
+                </div>
             </div>
-        {project.name}
+            <div>
+                Dashboard
+            </div>
         </div>
   );
 }
