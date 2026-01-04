124d9c47eed9c779573b787691df6ec13918722d
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-01-05 03:08:56 +0700

    ทำ hook ของ use-credential

---
+++ frontend-ce68-22/src/hooks/use-credential.ts
@@ -0,0 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
+import { credentialService } from "../services/credential.service";
+
+export function useCredential(id: number) {
+  return useQuery({
+    // Key ต้องมี ID เพื่อให้แยก cache ของแต่ละโปรเจกต์
+    queryKey: ["credential", id],
+    queryFn: () => credentialService.getById(id),
+    
+    // enabled: !!id คือถ้าไม่มี id (เช่นเป็น null/0) จะไม่ยิง API
+    enabled: !!id, 
+  });
+}
