ee7fe653f98c7e7fa104530e03099296bcf4cc96
Thapat Wattanaweeradej <captainthapat@gmail.com>
2026-01-22 16:30:58 +0700

    Revert "merged"
    This reverts commit 051ef0040da4c4466e4eb397786e03accceadea7, reversing
    changes made to 8f5363667e9c3500720f9e5c6177a383de298a74.

--- frontend-ce68-22/src/app/projects/[id]/(dashboard)/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation";
-import { use } from "react";
-
-export default function ProjectRootPage({ params }: { params: Promise<{ id: string }> }) {
-  const { id } = use(params);
-  
-  // สั่ง Redirect ทันทีที่เข้ามาหน้านี้
-  redirect(`/projects/${id}/overview`);
-}
--- frontend-ce68-22/src/types/access_key.ts
+++
@@ -1,4 +0,0 @@
-export interface AccessKey {
-    id: number;
-    key: string;
-}
