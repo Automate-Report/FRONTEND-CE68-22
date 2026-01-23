d0b68b479b1b1c19763780ae45afe45aba170688
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-01-23 19:54:42 +0700

    use localhost

--- frontend-ce68-22/src/services/auth.service.ts
+++ frontend-ce68-22/src/services/auth.service.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { LoginPayload, RegisterPayload } from "../types/auth";
 
 // const API = process.env.NEXT_PUBLIC_API_URL;
-const API = "http://127.0.0.1:8000"
+const API = "http://localhost:8000"
 
 const api = axios.create({
     baseURL: API,
