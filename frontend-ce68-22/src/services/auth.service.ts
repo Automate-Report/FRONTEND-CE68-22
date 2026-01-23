6af028813c03b9c564d28e4b513e7d849de22c26
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-01-23 19:02:18 +0700

    fix tailwind

--- frontend-ce68-22/src/services/auth.service.ts
+++ frontend-ce68-22/src/services/auth.service.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { LoginPayload, RegisterPayload } from "../types/auth";
 
-const API = process.env.NEXT_PUBLIC_API_URL;
+// const API = process.env.NEXT_PUBLIC_API_URL;
+const API = "http://127.0.0.1:8000"
 
 const api = axios.create({
     baseURL: API,
