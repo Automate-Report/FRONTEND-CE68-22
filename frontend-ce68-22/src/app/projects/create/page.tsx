2d0b975cbbbad39566bb6f2192c50f9070cef2f4
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-01-25 23:08:58 +0700

    generic style red & green button

--- frontend-ce68-22/src/app/projects/create/page.tsx
+++ frontend-ce68-22/src/app/projects/create/page.tsx
@@ -9,12 +9,10 @@ import { GenericBreadcrums } from "@/src/components/Common/GenericBreadCrums";
 import { TagManager } from "@/src/components/projects/TagManager";
 import CustomTextField from "@/src/components/Common/CustomTextField";
 
+
+
 import { muiGreenButtonStyle } from "@/src/styles/greenButton";
 import { muiRedButtonStyle } from "@/src/styles/redButton";
-
-// Mock Data
-const INITIAL_AVAILABLE_TAGS = ["Development", "Design", "Marketing", "Research", "Urgent"];
-
 
 export default function CreateProjectPage() {
   // เรียกใช้ Hook ตัวเดียวได้ครบทุกอย่าง
