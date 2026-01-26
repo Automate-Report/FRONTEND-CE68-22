65c3ce8fd98640f73a86f30777df126daaa61bca
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-01-27 00:05:42 +0700

    แก้ไขให้เหลือแต่ logic delete

--- frontend-ce68-22/src/app/workers/create/page.tsx
+++ frontend-ce68-22/src/app/workers/create/page.tsx
@@ -8,6 +8,7 @@ import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined'; // อย่า
 import { GenericBreadcrums } from "@/src/components/Common/GenericBreadCrums";
 import CustomTextField from "@/src/components/Common/CustomTextField";
 import { workerService } from "@/src/services/worker.service";
+import { getMe } from "@/src/services/auth.service";
 
 // --- Styles ---
 import { muiRedButtonStyle } from "@/src/styles/redButton";
@@ -20,6 +21,7 @@ export default function CreateWorkerPage() {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState<string | null>(null);
 
+
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
         if (!name.trim() || !threads) return;
@@ -30,7 +32,8 @@ export default function CreateWorkerPage() {
             
             await workerService.create({
                 name: name.trim(),
-                thread_number: Number(threads)
+                thread_number: Number(threads),
+                user_id: uid
             });
             router.push("/workers"); 
         } catch (err: any) {
