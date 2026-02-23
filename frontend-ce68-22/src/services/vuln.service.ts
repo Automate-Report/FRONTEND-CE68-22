eaf1e3b64249db8bce137fb7e4ce79598c91584c
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-02-23 21:07:45 +0700

    เพิ่มส่วนของการ get all vuln

--- frontend-ce68-22/src/services/vuln.service.ts
+++ frontend-ce68-22/src/services/vuln.service.ts
@@ -1,8 +1,27 @@
 import apiClient from "../lib/api-client";
-import { SummaryStatusVlun } from "../types/vuln";
+import { SummaryStatusVlun, VulnIssue } from "../types/vuln";
+import { PaginatedResult } from "../types/common";
 
 export const vulnService = {
+  getAll: async (projectId: number, page: number, size: number, sortBy?: string | null, sortOrder?: "asc" | "desc" | "none", search?: string | null, filter?: string | "ALL") => {
+    
+    // แปลงค่า sortOrder ให้เป็น string ที่ Backend เข้าใจ (ถ้าเป็น none ให้ส่ง undefined)
+    const orderParam = sortOrder === "none" ? undefined : sortOrder;
+    const sortParam = sortBy || undefined;
 
+    const { data } = await apiClient.get<PaginatedResult<Worker>>(`vuln/all/${projectId}`, {
+      params: {
+        project_id: projectId,
+        page,
+        size,
+        sort_by: sortParam, // ชื่อต้องตรงกับ Backend (FastAPI)
+        order: orderParam,
+        search,
+        filter
+      },
+    });
+    return data;
+  },
   cntByProjectId: async (projectId: number) => {
     const { data } = await apiClient.get<number>(`/vulns/cnt/${projectId}`);
     return data;
--- frontend-ce68-22/src/types/vuln.ts
+++ frontend-ce68-22/src/types/vuln.ts
@@ -5,3 +5,41 @@ export interface SummaryStatusVlun {
     in_progress_cnt: number;
     fixed_cnt: number;
 }
+
+interface ReProduceInfo {
+    target: string;
+    method: string;
+    payload: string;
+    curl_command: string;
+}
+
+interface Evidence {
+    screenshot: string;
+    response_details: string;
+}
+
+interface VulnDates {
+    first_seen: string;
+    last_seen: string;
+}
+
+interface CVSSDetails {
+    score: Float16Array;
+    vector: string;
+    version: string;
+}
+export interface VulnIssue {
+    id: number;
+    title: string;
+    asset_id: number;
+    asset_name: string;
+    assigned_to?: string;
+    verified_by?: string;
+    severity: string;
+    status: string;
+    occurrance_count: number;
+    cvss_detials: CVSSDetails;
+    reproduce_info: ReProduceInfo;
+    dates: VulnDates;
+    recommendation: string;
+}
