6cc09d85382fd313981f6128fc17446dbd7f8e23
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-02-26 15:11:05 +0700

    แก้ path backend

--- frontend-ce68-22/src/services/noti.service.ts
+++ frontend-ce68-22/src/services/noti.service.ts
@@ -3,7 +3,7 @@ import apiClient from "../lib/api-client";
 
 export const notiService = {
     getNoti: async (skip: number, limit: number, isUnread: boolean) => {
-        const { data } = await apiClient.get<Notification[]>("/notification", {
+        const { data } = await apiClient.get<Notification[]>("/notification/", {
             params: {
                 skip,
                 limit,
