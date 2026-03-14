edc9ab2062a6cd957298e7c3cf8d95036bcca82c
Nattharika-jeksungnern <nattharika25kata@gmail.com>
2026-03-14 20:49:43 +0700

    add search and filter asset

--- frontend-ce68-22/src/app/projects/[id]/(dashboard)/asset/page.tsx
+++ frontend-ce68-22/src/app/projects/[id]/(dashboard)/asset/page.tsx
@@ -3,13 +3,16 @@
 import { use, useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useProject } from "@/src/hooks/project/use-project";
+
 import { GenericBreadcrums } from "@/src/components/Common/GenericBreadCrums";
 import { GenericGreenButton } from "@/src/components/Common/GenericGreenButton";
+import { GenericFilterButton } from "@/src/components/Common/FilterButton";
+import SearchBox from "@/src/components/Common/GenericSearchBox";
 
 import { AssetList } from "@/src/components/assets/AssetList";
 
 import CreateAssetIcon from "@/src/components/icon/CreateAssertIcon";
-import SearchIcon from '@mui/icons-material/Search';
+
 
 interface PageProps{
     params: Promise<{ id: string}>;
@@ -18,9 +21,13 @@ interface PageProps{
 export default function ProjectsAssetsPage({ params }: PageProps) {
     // Search
     const [searchQuery, setSearchQuery] = useState("");
-    const [filterStatus, setFilterStatus] = useState("ALL");
+    const [statusFilter, setStatusFilter] = useState<string>("ALL");
 
-    const router = useRouter();
+    const filterOptions = [
+      { label: "All Type", value: "ALL" },
+      { label: "URL", value: "url" },
+      { label: "IP", value: "ip" },
+    ];
 
     const resolvePrams = use(params);
     const projectId = parseInt(resolvePrams.id);
@@ -35,7 +42,9 @@ export default function ProjectsAssetsPage({ params }: PageProps) {
         { label: project.name , href: undefined}
     ];
 
-
+    const handleFilterChange = (value: string) => {
+      setStatusFilter(value);
+    };
 
     return (
         <div className="flex flex-col w-full text-[#E6F0E6] max-w-7xl">
@@ -52,24 +61,19 @@ export default function ProjectsAssetsPage({ params }: PageProps) {
 
             <div className="my-6 flex justify-between">
               <div className="relative w-2/4">
-                {/*ตัว Icon: สั่ง absolute ให้ลอยอยู่ซ้าย และจัดกึ่งกลางแนวตั้ง */}
-                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                    <SearchIcon sx={{ color: "#E6F0E6" }} /> 
-                </div>
-
-                {/*ตัว Input: เพิ่ม pl-10 เพื่อเว้นที่ให้ Icon */}
-                <input
-                    type="text"
-                    placeholder="Search projects..."
-                    className="w-full bg-[#1A2023] border border-[#2A3033] rounded-lg pl-10 pr-4 py-2 text-[#E6F0E6]
-                              focus:outline-none 
-                            focus:border-[#8FFF9C]"
-                    value={searchQuery}
-                    onChange={(e) => setSearchQuery(e.target.value)}
+                <SearchBox 
+                  value={searchQuery} 
+                  onChange={setSearchQuery} 
+                  placeholder="Search Assets"
+                  className="w-full max-w-md"
                 />
               </div>
               <div className="flex gap-8 items-center">
-                <div>filter</div>
+                <GenericFilterButton 
+                  options={filterOptions} 
+                  currentValue={statusFilter} 
+                  onSelect={handleFilterChange} 
+                />
                 <GenericGreenButton 
                   name="New Asset" 
                   href={`/projects/${projectId}/asset/create`}
@@ -80,11 +84,10 @@ export default function ProjectsAssetsPage({ params }: PageProps) {
 
             <AssetList 
               searchQuery={searchQuery}
-              filterStatus={filterStatus}
+              filterStatus={statusFilter}
               project_id={projectId}
             />
-                
-
+            
         </div>
   );
 }
